2a987c613323cc04a452b99e3bc3215ba4029fa2
zain-mzb <comsatsbscs@gmail.com>
2021-04-20 00:29:36 +0500

    Express Sessions

--- expgenerator/conFusionServer/app.js
+++ expgenerator/conFusionServer/app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
@@ -32,38 +33,61 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser('12345-6789-09876-54321'));
+
+app.use(session({
+  name:'session-id',
+  secret: '12345-6789-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+function auth (req, res, next) {
+    console.log(req.session);
+
+  if(!req.session.user) {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
+  }
+  else {
+    if (req.session.user === 'authenticated') {
+      next();
+    }
+    else {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
+    }
+  }
+}
+
+
 
 function auth(req,res, next){
-  console.log(req.headers);
-
-  var authHeader = req.headers.authorization;
-
-  if(!authHeader){
+  console.log(req.session);
+ 
+  if(!req.session.user){
     var err = new Error('You are not autheticated!');
-
     res.setHeader('WWW-Authenticate', 'Basic');
     err.status = 401;
-
     return next(err);
   }
-  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-
-  var username = auth[0];
-  var password = auth[1];
-
-  if(username === 'admin' && password ==='password'){
+else{
+  if(req.session.user === 'authenticated'){
     next();
   }
   else{
     var err = new Error('You are not autheticated!');
-
-    res.setHeader('WWW-Authenticate', 'Basic');
-    err.status = 401;
-
+    err.status = 403;
     return next(err);
   }
-
+}
 }
 
 /////puting our authentication wall
@@ -72,8 +96,7 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
